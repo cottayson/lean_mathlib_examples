50fadc6f8b52db327dc5819a169e35b309cd59e2
cottayson <cottayson@gmail.com>
2021-01-13 01:54:48 +0700

    Fix error by adding k ≥ 1 to choose.empty

--- src/functional_equations/combine.lean
+++ src/functional_equations/combine.lean
@@ -94,10 +94,10 @@ axiom choose.step (k : ℕ) (x : α) (xs : list α) :
 attribute [simp] choose.zero choose.empty
 
 example : choose 0 [] = [[]] := choose.zero nil
-example : choose 1 [] = [] := by {
-  rw [choose.empty],
-  exact nat.less_than_or_equal.refl,
-}
+lemma one_nil   : choose 1 [] = [] := by rw [choose.empty]; repeat { apply nat.less_than_or_equal.refl <|> apply nat.less_than_or_equal.step }
+lemma two_nil   : choose 2 [] = [] := by rw [choose.empty]; repeat { apply nat.less_than_or_equal.refl <|> apply nat.less_than_or_equal.step }
+lemma three_nil : choose 3 [] = [] := by rw [choose.empty]; repeat { apply nat.less_than_or_equal.refl <|> apply nat.less_than_or_equal.step }
+
 example (x : α) : choose 0 [x] = [[]] := choose.zero [x]
 example (x : α) : choose 1 [x] = [[x]] := by {
   rw [ choose.step, choose.zero, choose.empty ],
@@ -106,23 +106,61 @@ example (x : α) : choose 1 [x] = [[x]] := by {
 }
 example (x : α) : choose 2 [x] = [] := by {
   simp [choose.step],
+  iterate 2 { rw choose.empty },
+  simp,
+  iterate 3 { apply nat.less_than_or_equal.refl <|> apply nat.less_than_or_equal.step },
 }
 
 #check nat.less_than_or_equal.step
 
 namespace vars2
-variables (x y z : α) (xs ys zs : list α)
+variables (x y z w : α) (xs ys zs ws : list α)
 -- choose (k+1) (x:xs) = map (x:) (choose k xs) ++ choose (k+1) xs
 -- choose 1 [x] = map (x:) (choose 0 []) ++ choose 1 [] = map (x:) [] ++ [] = []
 
 -- choose k (x:xs) = map (x:) (choose (k-1) xs) ++ choose k xs
 -- choose 1 [x] = map (x:) (choose 0 []) ++ choose 1 [] = 
-example : choose 1 [x]       = [[x]]         := by simp [choose.step, nat.le_refl, nat.less_than_or_equal.step]
-example : choose 1 [x, y]    = [[x]]      := by simp [choose.step]
-example : choose 1 [x, y, z] = [[x], [y]] := by simp [choose.step]
-example : choose 1 [x, y, z] = [[x], [y]] := by iterate 3 { rw choose.step, simp }
+example : choose 1 [x] = [[x]] := by {
+  simp [choose.step]; rw [choose.empty],
+  apply nat.less_than_or_equal.refl <|> apply nat.less_than_or_equal.step,
+}
 
-example : choose 2 [x, y] = [[x, y]]                     := by simp [choose.step]
-example : choose 3 [x, y] = [[x, x, x], [x, x, y], [x, y, y], [y, y, y]] := by simp [choose.step]
+example : choose 1 [x]       = [[x]]           := by simp [choose.step, one_nil]
+example : choose 1 [x, y]    = [[x], [y]]      := by simp [choose.step, one_nil]
+example : choose 1 [x, y, z] = [[x], [y], [z]] := by simp [choose.step, one_nil]
+-- example : choose 1 [x, y, z] = [[x], [y]] := by iterate 3 { rw choose.step, simp }
+
+-- induction forcing method:
+-- 1. find symmetries, rules, shortest rules (requires minimum meta-rules that can generate rules), optimal path (list) of rules, "optimal path (method)" of calculating optimal paths(lists)...
+-- 2. create set of hypothesis
+-- 3. prove it or find a structure that simplifies task of proof in more general cases
+
+-- group of steps for forcing cool_lemma1
+example : choose 0 [] = [[]] := by simp [choose.step]
+example : choose 1 [x] = [[x]] := by simp [choose.step, one_nil]
+example : choose 2 [x, y] = [[x, y]] := by simp [choose.step, one_nil, two_nil]
+example : choose 3 [x, y, z] = [[x, y, z]] := by simp [choose.step, one_nil, two_nil, three_nil]
+lemma cool_lemma1 (n : ℕ): choose n xs = [xs] := sorry
+
+example : choose 3 [x, y] = [] := by {
+  repeat {
+    simp [choose.step, one_nil],
+    rw [choose.empty]; repeat { apply nat.less_than_or_equal.refl <|> apply nat.less_than_or_equal.step },
+  },
+}
+
+example : choose 2 [x, y, z, w] = [[x,y],[x,z],[x,w],[y,z],[y,w],[z,w]] := by {
+  repeat {
+    simp [choose.step, one_nil],
+    rw [choose.empty]; repeat { apply nat.less_than_or_equal.refl <|> apply nat.less_than_or_equal.step },
+  },
+}
+
+example : choose 3 [x, y, z, w] = [[x,y,z],[x,y,w],[x,z,w],[y,z,w]] := by {
+  repeat {
+    simp [choose.step, one_nil],
+    rw [choose.empty]; repeat { apply nat.less_than_or_equal.refl <|> apply nat.less_than_or_equal.step },
+  },
+}
 
 end vars2
